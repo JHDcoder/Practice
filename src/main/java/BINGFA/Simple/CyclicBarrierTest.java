efccab4b250f596a7c82ff960ba0a73d9a129f5f
FwcCC <1850491992@qq.com>
2019-04-03 23:56:28 +0800

    Test

---
+++ src/main/java/BINGFA/Simple/CyclicBarrierTest.java
@@ -0,0 +1,34 @@
+package BINGFA.Simple;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierTest {
+    static CyclicBarrier cyclicBarrier=new CyclicBarrier(2);
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(1);
+            }
+        }).start();
+
+
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println(2);
+    }
+}
