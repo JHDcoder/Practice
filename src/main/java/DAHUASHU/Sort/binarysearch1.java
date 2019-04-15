1df6d632be27fcab296310f98dcc8bfaa35cdb2b
FwcCC <15623454836@126.com>
2019-04-15 12:15:54 +0800

    binarysearch4

--- src/main/java/DAHUASHU/Sort/binarysearch1.java
+++ src/main/java/DAHUASHU/Sort/binarysearch1.java
@@ -29,7 +29,7 @@ public class binarysearch1 {
         }
         return -1;
     }
-    //1.查找最后一个值等于给定值得元素
+    //2.查找最后一个值等于给定值得元素
     public int bsearch2(int [] a,int n,int value){
         int low=0;
         int high=n-1;
@@ -43,6 +43,40 @@ public class binarysearch1 {
             }else{
                if((mid==n-1)||a[mid+1]!=value) return mid;
                else low=mid+1;
+            }
+        }
+        return -1;
+    }
+
+    //3.查找第一个大于等于等于给定值得元素
+    public int bsearch3(int [] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] >= value) {
+                if ((mid == 0) || a[mid - 1] < value) return mid;
+                else high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    //4.查找最后一个小于等于给定值得元素
+    public int bsearch4(int [] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high=mid-1;
+            } else {
+                if((mid==n-1)||a[mid+1]>value) return mid;
+                else low=mid+1;
             }
         }
         return -1;
