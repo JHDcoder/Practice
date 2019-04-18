713c21aaa0b368913b41ecc0754fc10bf877700c
FwcCC <15623454836@126.com>
2019-04-18 16:41:12 +0800

    dfs

--- src/main/java/DAHUASHU/Graph/Graph.java
+++ src/main/java/DAHUASHU/Graph/Graph.java
@@ -60,4 +60,35 @@ public class Graph {
         }
         System.out.println(t+" ");
     }
+
+
+    boolean found=false;//全局变量或者类变量
+
+    public void dfs(int s,int t){
+        found=false;
+        boolean [] visited=new boolean[v];
+        int [] prev=new int [v];
+        for(int i=0;i<v;i++){
+            prev[i]=-1;
+        }
+        recurDfs(s,t,visited,prev);
+        print(prev,s,t);
+    }
+
+    private void recurDfs(int w,int t,boolean [] visited,int [] prev){
+        if(found==true) return;
+        visited[w]=true;
+        if(w==t){
+            found=true;
+            return;
+        }
+        for(int i=0;i<adj[w].size();i++){
+            int q=adj[w].get(i);
+            if(!visited[q]){
+                prev[q]=w;
+                recurDfs(q,t,visited,prev);
+            }
+        }
+    }
+
 }
