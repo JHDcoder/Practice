3e3ed0844ebc29197c2330d1a03cdeb0da94b956
FwcCC <15623454836@126.com>
2019-05-12 16:27:11 +0800

    bst

---
+++ src/main/java/DAHUASHU/Tree/BST.java
@@ -0,0 +1,72 @@
+package DAHUASHU.Tree;
+
+public class BST<E extends Comparable<E>>  {
+    private class Node{
+        public E e;
+        public Node left,right;
+        public Node(E e){
+            this.e=e;
+            left=null;
+            right=null;
+        }
+    }
+    private Node root;
+    private int size;
+    public BST(){
+        root=null;
+        size=0;
+    }
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+
+    public void add(E e){
+
+        if(root==null){
+            root=new Node(e);
+            size++;
+        }
+        else
+           root=add(root,e);
+
+    }
+    //递归算法
+    private Node add(Node node,E e){
+        /*if(e.equals(node.e))
+            return;
+        else if(e.compareTo(node.e)<0&&node.left==null){
+            node.left=new Node(e);
+            size++;
+            return;
+        }
+        else if(e.compareTo(node.e)>0&&node.right==null){
+            node.right=new Node(e);
+            size++;
+            return;
+        }*/
+
+        if(node==null){
+            size++;
+            return new Node(e);
+        }
+
+        if(e.compareTo(node.e)<0)
+            node.left=add(node.left,e);
+        else if(e.compareTo(node.e)>0)
+            node.right=add(node.right,e);
+
+        return node;
+
+    }
+
+
+
+
+
+
+}
