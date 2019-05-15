d7fbd1c64bb7d57edbc5c285d77f3e1ecc3e2bb7
FwcCC <15623454836@126.com>
2019-05-15 14:12:27 +0800

    segmenttree

--- src/main/java/DAHUASHU/SegmentTree/SegmentTree.java
+++ src/main/java/DAHUASHU/SegmentTree/SegmentTree.java
@@ -4,12 +4,33 @@ public class SegmentTree<E> {
 
     private E [] tree;
     private E [] data;
-    public SegmentTree(E [] arr){
+    private Merger<E> merger;
+    public SegmentTree(E [] arr,Merger<E> merger){
+        this.merger=merger;
         data=(E [])new Object[arr.length];
         for(int i=0;i<arr.length;i++)
             data[i]=arr[i];
 
         tree=(E [])new Object[4*arr.length];
+        buildSegmentTree(0,0,data.length-1);
+    }
+
+    //[l....r]的线段树
+    private void buildSegmentTree(int treeIndex,int l,int r){
+        if(l==r){
+            tree[treeIndex]=data[l];
+            return;
+        }
+
+        int leftTreeIndex=leftChild(treeIndex);
+        int rightTreeIndex=rightChild(treeIndex);
+
+        int mid=l+(r-l)/2;
+        buildSegmentTree(leftTreeIndex,l,mid);
+        buildSegmentTree(rightTreeIndex,mid+1,r);
+
+        tree[treeIndex]=merger.merge(tree[leftTreeIndex],tree[rightTreeIndex]);
+
     }
 
 
@@ -29,6 +50,48 @@ public class SegmentTree<E> {
 
     private int rightChild(int index){
         return 2*index+2;
+    }
+
+    public E query(int queryL,int queryR){
+        if(queryL<0||queryL>=data.length||queryR<0||queryR>=data.length||queryL>queryR)
+            throw new IllegalArgumentException("index is illegal");
+        return query(0,0,data.length-1,queryL,queryR);
+    }
+
+    private E query(int treeIndex,int l,int r,int querL,int queryR){
+        if(l==querL&&r==queryR){
+            return tree[treeIndex];
+        }
+
+        int mid=l+(r-l)/2;
+        int leftTreeIndex=leftChild(treeIndex);
+        int rightTreeIndex=rightChild(treeIndex);
+
+        if(querL>=mid+1)
+            return query(rightTreeIndex,mid+1,r,querL,queryR);
+        else if(queryR<=mid)
+            return query(leftTreeIndex,l,mid,querL,queryR);
+        E leftresult=query(leftTreeIndex,l,mid,querL,mid);
+        E rightresult=query(rightTreeIndex,mid+1,r,mid+1,queryR);
+        return merger.merge(leftresult,rightresult);
+
+
+
+    }
+
+    public String toString(){
+        StringBuilder res=new StringBuilder();
+        res.append('[');
+        for(int i=0;i<tree.length;i++){
+            if(tree[i]!=null)
+                res.append(tree[i]);
+            else
+                res.append("null");
+            if(i!=tree.length-1)
+                res.append(", ");
+        }
+        res.append(']');
+        return res.toString();
     }
 
 
